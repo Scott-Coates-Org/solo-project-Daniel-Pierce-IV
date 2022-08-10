d31d0e9673cd10d6b8774cddac19e41532219a1d
Daniel Pierce <daniel.pierce.iv@gmail.com>
2022-08-10 12:03:32 -0500

    Create RecipeCards

---
+++ src/components/RecipeCard.jsx
@@ -0,0 +1,15 @@
+export default function RecipeCard({
+  recipe: { name, description, imageURL },
+}) {
+  return (
+    <div className="flex flex-col w-60 bg-gray-100 mb-3 p-3">
+      <img
+        className="aspect-video bg-gray-300"
+        src={imageURL}
+        alt={`Image of ${name}`}
+      />
+      <h1 className="text-center border-b border-gray-700">{name}</h1>
+      <p>{description}</p>
+    </div>
+  );
+}
--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,17 +1,13 @@
 import { collection } from 'firebase/firestore';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 import { db } from '../../firebase/client';
+import RecipeCard from '../RecipeCard';
 
 export default function Homepage({}) {
   const [recipes] = useCollectionData(collection(db, 'recipes'));
 
   return (
     recipes &&
-    recipes.map((recipe, index) => (
-      <div className="mb-3" key={index}>
-        <div>{recipe.name}</div>
-        <div>{recipe.description}</div>
-      </div>
-    ))
+    recipes.map((recipe, index) => <RecipeCard recipe={recipe} key={index} />)
   );
 }
