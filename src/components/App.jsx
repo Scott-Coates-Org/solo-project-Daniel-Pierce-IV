a62cf4a3d69430d09406881ebcaf47ee47c0048a
Daniel Pierce <daniel.pierce.iv@gmail.com>
2022-08-10 15:30:38 -0500

    Implement AdminPage for easy recipe creation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,14 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AdminPage from './pages/AdminPage';
 import Homepage from './pages/HomePage';
 
 export default function App() {
   return (
-    <div>
-      <Homepage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/admin" element={<AdminPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
