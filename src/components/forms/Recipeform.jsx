9c6a9c6f2e7c2d8da0d3bc27fcdb1e1eb1adcf9c
Daniel Pierce <daniel.pierce.iv@gmail.com>
2022-08-10 18:59:04 -0500

    Refactor the recipe form as component

---
+++ src/components/forms/Recipeform.jsx
@@ -0,0 +1,88 @@
+import { addDoc, collection, setDoc } from 'firebase/firestore';
+import { getDownloadURL, ref } from 'firebase/storage';
+import { useState } from 'react';
+import { useUploadFile } from 'react-firebase-hooks/storage';
+import { db, storage } from '../../firebase/client';
+
+export default function RecipeForm({}) {
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [image, setImage] = useState(null);
+  const [uploadFile] = useUploadFile();
+
+  async function createRecipe(e) {
+    e.preventDefault();
+
+    if (image) {
+      try {
+        const recipeData = {
+          name: name,
+          description,
+        };
+
+        const recipeRef = await addDoc(collection(db, 'recipes'), recipeData);
+
+        const imageRef = ref(storage, `${recipeRef.id}/card`);
+
+        const result = await uploadFile(imageRef, image);
+
+        await setDoc(recipeRef, {
+          ...recipeData,
+          imageURL: await getDownloadURL(imageRef),
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+
+  return (
+    <form
+      action=""
+      className="flex flex-col w-96 border p-3"
+      onSubmit={createRecipe}
+    >
+      <h1 className="text-2xl">Add recipe to Database</h1>
+
+      <div>
+        <label htmlFor="recipe-name">Name</label>
+        <input
+          type="text"
+          name="recipeName"
+          id="recipe-name"
+          className="border w-full"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="recipe-description">Description</label>
+        <textarea
+          className="border w-full"
+          name="recipeDescription"
+          id="recipe-description"
+          cols="30"
+          rows="10"
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
+        ></textarea>
+      </div>
+
+      <div>
+        <label htmlFor="recipe-image">Name</label>
+        <input
+          type="file"
+          name="recipeImage"
+          id="recipe-image"
+          className="border w-full"
+          onChange={(e) => setImage(e.target.files[0])}
+        />
+      </div>
+
+      <button type="submit" className="self-end bg-green-500 px-2 mt-4">
+        Create Recipe
+      </button>
+    </form>
+  );
+}
--- src/components/pages/AdminPage.jsx
+++ src/components/pages/AdminPage.jsx
@@ -1,19 +1,13 @@
-import { addDoc, collection, setDoc } from 'firebase/firestore';
-import { getDownloadURL, ref } from 'firebase/storage';
+import { addDoc, collection } from 'firebase/firestore';
 import { useState } from 'react';
-import { db, storage } from '../../firebase/client';
-import { useUploadFile } from 'react-firebase-hooks/storage';
+import { db } from '../../firebase/client';
 import { Ingredients } from '../../firebase/models/ingredients';
 import { useEffect } from 'react';
+import RecipeForm from '../forms/Recipeform';
 
 export default function AdminPage({}) {
-  const [recipeName, setRecipeName] = useState('');
-  const [description, setDescription] = useState('');
   const [ingredientName, setIngredientName] = useState('');
   const [ingredients, setIngredients] = useState([]);
-
-  const [image, setImage] = useState(null);
-  const [uploadFile] = useUploadFile();
 
   function fetchIngredients() {
     Ingredients.all().then((data) => setIngredients(data));
@@ -22,32 +16,6 @@ export default function AdminPage({}) {
   useEffect(() => {
     fetchIngredients();
   }, []);
-
-  async function createRecipe(e) {
-    e.preventDefault();
-
-    if (image) {
-      try {
-        const recipeData = {
-          name: recipeName,
-          description,
-        };
-
-        const recipeRef = await addDoc(collection(db, 'recipes'), recipeData);
-
-        const imageRef = ref(storage, `${recipeRef.id}/card`);
-
-        const result = await uploadFile(imageRef, image);
-
-        await setDoc(recipeRef, {
-          ...recipeData,
-          imageURL: await getDownloadURL(imageRef),
-        });
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  }
 
   async function createIngredient(e) {
     e.preventDefault();
@@ -72,53 +40,7 @@ export default function AdminPage({}) {
 
   return (
     <div className="flex justify-center items-start gap-5">
-      <form
-        action=""
-        className="flex flex-col w-96 border p-3"
-        onSubmit={createRecipe}
-      >
-        <h1 className="text-2xl">Add recipe to Database</h1>
-
-        <div>
-          <label htmlFor="recipe-name">Name</label>
-          <input
-            type="text"
-            name="recipeName"
-            id="recipe-name"
-            className="border w-full"
-            onChange={(e) => setRecipeName(e.target.value)}
-            value={recipeName}
-          />
-        </div>
-
-        <div>
-          <label htmlFor="recipe-description">Description</label>
-          <textarea
-            className="border w-full"
-            name="recipeDescription"
-            id="recipe-description"
-            cols="30"
-            rows="10"
-            onChange={(e) => setDescription(e.target.value)}
-            value={description}
-          ></textarea>
-        </div>
-
-        <div>
-          <label htmlFor="recipe-image">Name</label>
-          <input
-            type="file"
-            name="recipeImage"
-            id="recipe-image"
-            className="border w-full"
-            onChange={(e) => setImage(e.target.files[0])}
-          />
-        </div>
-
-        <button type="submit" className="self-end bg-green-500 px-2 mt-4">
-          Create Recipe
-        </button>
-      </form>
+      <RecipeForm />
 
       <form
         action=""
