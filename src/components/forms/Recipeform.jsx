152e3d5292463ef92bb51e0d7d5a5af6ba435cde
Daniel Pierce <daniel.pierce.iv@gmail.com>
2022-08-13 18:28:14 -0500

    Implement adding Ingredients to Recipe form

--- src/components/forms/Recipeform.jsx
+++ src/components/forms/Recipeform.jsx
@@ -5,16 +5,23 @@ export default function RecipeForm({}) {
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
   const [imageFile, setImageFile] = useState(null);
+  const [ingredients, setIngredients] = useState([]);
+  const [ingredientName, setIngredientName] = useState('');
 
   function createRecipe(e) {
     e.preventDefault();
-    Recipe.add({ name, description, imageFile });
+    Recipe.add({ name, description, imageFile, ingredients });
+  }
+
+  function addIngredient() {
+    setIngredients([...ingredients, ingredientName]);
+    setIngredientName('');
   }
 
   return (
     <form
       action=""
-      className="flex flex-col w-96 border p-3"
+      className="flex flex-col w-96 border p-3 gap-3"
       onSubmit={createRecipe}
     >
       <h1 className="text-2xl">Add recipe to Database</h1>
@@ -53,6 +60,49 @@ export default function RecipeForm({}) {
           className="border w-full"
           onChange={(e) => setImageFile(e.target.files[0])}
         />
+      </div>
+
+      <ul className="border-4 p-2">
+        <li>Ingredients:</li>
+        {ingredients.map((ingredient) => (
+          <li
+            key={ingredient}
+            onClick={() =>
+              setIngredients(
+                ingredients.filter((element) => element !== ingredient)
+              )
+            }
+          >
+            {ingredient}
+          </li>
+        ))}
+      </ul>
+
+      <div>
+        <label htmlFor="ingredient-name">Ingredient Name</label>
+        <div className="flex">
+          <input
+            type="text"
+            name="ingredientName"
+            id="ingredient-name"
+            className="border w-full"
+            onChange={(e) => setIngredientName(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                e.preventDefault();
+                addIngredient();
+              }
+            }}
+            value={ingredientName}
+          />
+          <button
+            type="button"
+            className="bg-red-300 px-2"
+            onClick={addIngredient}
+          >
+            Add
+          </button>
+        </div>
       </div>
 
       <button type="submit" className="self-end bg-green-500 px-2 mt-4">
