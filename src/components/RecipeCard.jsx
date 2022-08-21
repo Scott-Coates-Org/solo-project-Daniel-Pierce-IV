1773439052be375a75c1b5d6ad816b594941fdd9
Daniel Pierce <daniel.pierce.iv@gmail.com>
2022-08-20 19:11:16 -0500

    Style RecipeCards

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -4,16 +4,21 @@ export default function RecipeCard({
   recipe: { id, name, description, imageURL },
 }) {
   return (
-    <Link to={`/recipes/${id}`}>
-      <div className="flex flex-col w-60 bg-gray-100 p-3">
+    <Link
+      to={`/recipes/${id}`}
+      className="group flex flex-col gap-2 text-gray-300 w-44 p-4 pt-0 rounded-[30px] transition-colors bg-recipe-gray-light hover:bg-recipe-gray-lighter "
+    >
+      {/* adjust image-to-card-background ratio with margin */}
+      <div className="flex justify-center mx-2">
         <img
-          className="aspect-video bg-gray-300"
+          className="relative w-1/2 aspect-square rounded-full scale-[200%] transition-transform -translate-y-1/2 group-hover:-translate-y-2/3"
           src={imageURL}
           alt={`Image of ${name}`}
         />
-        <h1 className="text-center border-b border-gray-700">{name}</h1>
-        <p>{description}</p>
       </div>
+
+      <h1>{name}</h1>
+      <p className="text-sm">{description}</p>
     </Link>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'recipe-gray-light': '#22222A',
+        'recipe-gray-lighter': '#32323A',
+      },
+    },
   },
   plugins: [],
 };
